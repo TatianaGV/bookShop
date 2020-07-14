bff21c807fe2e4818020d8e91c0f2cf99c1787d2
TatianaGV <tanyanahaus@yandex.ru>
2020-07-15 00:15:57 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
+import { MainPageComponent } from './layout/components/main-page/main-page.component';
+import { AuthorsMainPageComponent } from './authors/component/authors-main-page/authors-main-page.component';
+import { BooksMainPageComponent } from './books/component/books-main-page/books-main-page.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: MainPageComponent },
+      { path: 'authors', component: AuthorsMainPageComponent },
+      { path: 'books', component: BooksMainPageComponent },
+    ],
   },
 
 ];
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BooksMainPageComponent } from './component/books-main-page/books-main-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BooksMainPageComponent],
   imports: [
     CommonModule
   ]
