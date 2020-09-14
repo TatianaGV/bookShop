d1aa3494e06fa5ab6b32c86e3e0104a57d8012b0
TatianaGV <tanyanahaus@yandex.ru>
2020-09-14 20:05:06 +0300

    feature: add date control

--- src/app/core/helpers/data-cross-validation.ts
+++ src/app/core/helpers/data-cross-validation.ts
@@ -1,6 +1,15 @@
 import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
 
 export const dataCrossValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
-  return  false ? { dataCheck: true } : null;
+  const writingDateControl = control.get('writingDate');
+  const releaseDateControl = control.get('releaseDate');
+
+  const checkDate = writingDateControl.value < releaseDateControl.value;
+  if (checkDate) {
+    writingDateControl.setErrors({ dataCheck: true });
+    releaseDateControl.setErrors({ dataCheck: true });
+  }
+
+  return checkDate ? { dataCheck: true } : null;
 };
 
