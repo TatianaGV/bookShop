3180c197b8c82aa12a41b0df91fcf487d911f1a2
TatianaGV <tanyanahaus@yandex.ru>
2020-09-18 12:38:40 +0300

    feature: template credit card

---
+++ src/app/libs/credit-card/credit-card.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { CreditCardComponent } from './credit-card/credit-card.component';
+
+
+@NgModule({
+  declarations: [
+    CreditCardComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    CreditCardComponent,
+  ],
+})
+export class CreditCardModule { }
